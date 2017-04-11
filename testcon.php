4fd4fb09f55034a0069626e0c5c3645ee6c0f167
skkojira <skkojira@users.noreply.github.com>
2017-04-11 12:11:23 +0700

    Update testcon.php

--- testcon.php
+++ testcon.php
@@ -1,7 +1,11 @@
 <?php
+echo "connect<br>";
 mysql_connect("180.183.246.189:8306","root","888ehong") or die(mysql_error());
+echo "select db<br>";
 mysql_select_db("repair");  
+echo "set utf8<br>";
 mysql_query("set names utf8");
+echo "select count<br>";
 $objrp=mysql_query("select count(*) as branch from branch group by br_id");
 $rsrp=mysql_fetch_assoc($objrp);
 mysql_close();
