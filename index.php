df6b696fb2db794a29ca0fccb993012d9f4bb62c
skkojira <skkojira@users.noreply.github.com>
2017-04-11 12:09:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-mysql_connect("180.183.246.189:8306","root","888ehong") or die(mysql_error());
-mysql_select_db("repair");  
-mysql_query("set names utf8");
-$objrp=mysql_query("select count(*) as branch from branch group by br_id");
-$rsrp=mysql_fetch_assoc($objrp);
-mysql_close();
-echo $rsrp['branch'];
+echo "Hi PHP,This is bot line";
 ?>
